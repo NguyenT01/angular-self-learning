355ae93cf9dc5e1ba39ae5394b4b8c906c5aecb9
nguyen <transinguyen2001@gmail.com>
2025-02-16 21:55:17 +0700

    Initialize Angular project with basic structure and configuration files

---
+++ ponyracer/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ns-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  standalone: true
+})
+export class AppComponent {
+  title = 'ponyracer';
+}
