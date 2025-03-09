897c9e63fc84826f548f67406a52267e02a8d58e
nguyen <transinguyen2001@gmail.com>
2025-03-09 17:11:27 +0700

    Add pony model and enhance races component with pipes and dynamic pony display

--- ponyracer/src/app/races/races.component.ts
+++ ponyracer/src/app/races/races.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import RaceModel from '../../models/race-model';
 import { CommonModule } from '@angular/common';
+import { PonyModel } from '../../models/pony-model';
 
 @Component({
   selector: 'ns-races',
@@ -17,13 +18,27 @@ export class RacesComponent {
 
   races: Array<RaceModel> = []
 
+  ponies =  new Map<number, PonyModel>();
+
+  // 30th January 2025, 14:30:00
+  sampleDate: Date = new Date(2025,0,30, 14,30,0);
+
   fontWeight: string = 'bold';
   color: string = 'black';
   isAwsome: boolean = true;
 
   refreshRaces(): void{
-    this.races = [{id: 1, name: 'London'}, {id: 2, name: 'Paris'}];
+    this.races = [{id: 1, name: 'London'}, {id: 4, name: 'Paris'}, {id: 3 , name: 'Tokyo'}, {id: 2, name: 'Shanghai'}]; ;
+
+    this.setPonies();
   }
+
+  setPonies(){
+    this.ponies.set(66, {name: 'Rainbow Dash'})
+    this.ponies.set(60, {name: 'Pinkie Pie'})
+    this.ponies.set(61, {name: 'Fluttershy'})
+  }
+
 
   addNewRace(){
     this.newRaceAvailable.emit('New race Added');
