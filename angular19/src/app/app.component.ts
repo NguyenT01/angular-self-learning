f9bfcbdaa5dfbd863277f58eca0f40e0bdd829dc
nguyen <transinguyen2001@gmail.com>
2025-04-07 11:14:52 +0700

    Implement PonyModel interface and enhance AppComponent with signal functionality

--- angular19/src/app/app.component.ts
+++ angular19/src/app/app.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { PonyModel } from '../models/pony-model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    JsonPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular19';
+
+  readonly count = signal(40);
+
+  readonly rainbowDash = signal<PonyModel>({
+    name: 'Rainbow Dash',
+    color: 'blue'
+  })
+
+  readonly pinkiePie: WritableSignal<PonyModel> = signal({
+    name: 'Pinkie Pie',
+    color: 'red'
+  })
+
+  readonly unknownPony = signal<PonyModel | undefined>(undefined);
+
+  //READ SIGNAL
+  getSignal(){
+    const value: PonyModel = this.rainbowDash();
+    console.log(value);
+  }
+  
+  // SET A NEW VALUE VALUE OF SIGNALS
+ setNewValueInSignal(){
+    this.rainbowDash.set({
+      name: 'Rainbow Dash',
+      color: 'yellow'
+    })
+  }
+
+  // UPDATE A VALUE OF SIGNALS
+  updateValueInSignal(){
+    this.rainbowDash.update(pony => ({...pony, color: 'green'}));
+
+    // 2nd way to update
+    this.rainbowDash.set({...this.rainbowDash(), color: 'ocean blue'})
+
+    // SIGNAL WITH ARRAY
+    const num = signal<number[]>([1, 2, 3]);
+    num.update((value) => [...value, 4])
+    console.log(num());
+  }
 }
