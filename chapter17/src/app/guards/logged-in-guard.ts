583f0b0e7502f95e2c11d343d9577ebd280f6696
nguyen <transinguyen2001@gmail.com>
2025-05-11 04:19:23 +0700

    refactor: improve formatting and readability of logged-in guard

--- chapter17/src/app/guards/logged-in-guard.ts
+++ chapter17/src/app/guards/logged-in-guard.ts
@@ -1,13 +1,19 @@
-import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { UserService } from "../services/user-service";
-import { inject } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { UserService } from '../services/user-service';
+import { inject } from '@angular/core';
 
 export const loggedInGuard: CanActivateFn = (
-    _route: ActivatedRouteSnapshot,
-    _state: RouterStateSnapshot
-) : boolean | UrlTree =>{
-    const userService = inject(UserService);
-    const router = inject(Router);
+  _route: ActivatedRouteSnapshot,
+  _state: RouterStateSnapshot
+): boolean | UrlTree => {
+  const userService = inject(UserService);
+  const router = inject(Router);
 
-    return userService.isLoggedIn() || router.parseUrl('/login');
-}
+  return userService.isLoggedIn() ?? router.parseUrl('/login');
+};
